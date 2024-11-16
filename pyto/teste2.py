4f22381dd878d94282098e999df5e475d313b3e9
cleannsz <zteuzynff@gmail.com>
2024-11-16 13:12:15 -0300

    Add files via upload

---
+++ pyto/teste2.py
@@ -0,0 +1,4 @@
+Dia = input('Dia = ')
+mes = input('Mes = ')
+Ano = input('Ano = ')
+print('Você nasceu no dia ' + Dia + ' de '+ mes + ' de ' + Ano + '. Correto?')
---
+++ pyto/teste3.py
@@ -0,0 +1,3 @@
+n1 = int(input('N1 = '))
+n2 = int(input('N2 = '))
+print('A soma é:' , n1 + n2) 
