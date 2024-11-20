1ec6be84d7e0c36ceed771cb573bcb1b8fa83859
cleannsz <zteuzynff@gmail.com>
2024-11-20 11:21:14 -0300

    Add files via upload

---
+++ pyto/PythonExercicios/ex004.py
@@ -0,0 +1,9 @@
+n = input('Digite algo: ')
+print('tipo: ', type(n))
+print('É alfanumerico? ', n.isnumeric())
+print('É alfabetico? ', n.isalpha())
+print('É numero? ', n.isalnum())
+print('Está em maiusculo? ', n.isupper())
+print('Está em minusculo? ', n.islower())
+print('Está capitalizada? ', n.istitle())
+
